020ef84c96ec488faf00330f590ecd9563ce5672
Pilar Luiz <pluiz@usc.edu>
2024-04-22 20:44:06 -0700

    Basic components in; TODO: Get favicons

--- pilarluiz/src/App.js
+++ pilarluiz/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

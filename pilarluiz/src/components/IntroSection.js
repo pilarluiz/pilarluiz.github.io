85f9d0d4d8c9f3588b63f44088f1a3d4a4f15c4c
Pilar Luiz <pluiz@usc.edu>
2025-02-08 14:35:12 -0800

    basic intro section

--- pilarluiz/src/components/IntroSection.js
+++ pilarluiz/src/components/IntroSection.js
@@ -5,7 +5,24 @@ import "./IntroSection.css";
 // import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 
 function IntroSection() {
-  return <div className="intro-container">IntroSection</div>;
+  return (
+    <div className="pink-background">
+      <div className="intro-container">
+        <h1 className="intro-heading">
+          Hello
+          <br />
+          World!
+        </h1>
+        <h3 className="name">std::string name = "pilar luiz";</h3>
+        <p className="intro-p">
+          I'm a computer engineer passionate about the intersection of hardware
+          and software, with a particular interest in computer architecture.
+          I've worked in industry across computer architecture, software
+          engineering, and robotics.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default IntroSection;

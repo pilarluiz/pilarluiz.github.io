31c2371a1eb65c8361fa2204b0beb90675f2f774
Pilar Luiz <pluiz@usc.edu>
2025-02-24 22:04:00 -0800

    intro section looking yummy

--- pilarluiz/src/components/IntroSection.js
+++ pilarluiz/src/components/IntroSection.js
@@ -7,23 +7,31 @@ import "./IntroSection.css";
 function IntroSection() {
   return (
     <div className="intro-container">
-      <div className="heading-container">
-        <div>
-          <h1 className="intro-heading">
-            Hello,
-            <br />
-            World!
-          </h1>
-          <h3 className="name">std::string name = "Pilar";</h3>
+      <div className="inner-intro-container">
+        <div className="heading-container">
+          <div>
+            <h1 className="intro-heading">
+              Hello,
+              <br />
+              World!
+            </h1>
+            <h3 className="name">std::string name = "Pilar";</h3>
+          </div>
+          <p className="intro-p">
+            I'm a <b>computer engineer</b> passionate about the intersection of
+            hardware and software, with a particular interest in computer
+            architecture. I've worked in industry across computer architecture,
+            software engineering, and robotics.
+          </p>
         </div>
-        <p className="intro-p">
-          I'm a <b>computer engineer</b> passionate about the intersection of
-          hardware and software, with a particular interest in computer
-          architecture. I've worked in industry across computer architecture,
-          software engineering, and robotics.
-        </p>
+        <div className="intro-image">
+          <img
+            src="images/pilar-headshot.JPG"
+            alt="Pilar Headshot"
+            className="headshot"
+          />
+        </div>
       </div>
-      <div className="intro-image">Image Placeholder</div>
     </div>
   );
 }

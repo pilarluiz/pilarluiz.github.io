85f3da26ebc1316e41f92f8da087836202eb35bd
Pilar Luiz <pluiz@usc.edu>
2025-08-22 17:15:07 -0500

    Experience section edited

--- pilarluiz/src/components/ExperienceSection.js
+++ pilarluiz/src/components/ExperienceSection.js
@@ -34,17 +34,26 @@ function ExperienceSection() {
   const experiences = [
     {
       id: 1,
-      company: "Applied Invention",
-      role: "Software Engineer",
-      dates: "Aug 2023 - Present",
+      company: "NVIDIA (2025)",
+      role: "Tegra Systems Architecture Intern",
+      dates: "May 2025 - Aug 2025",
       content: [
-        "Designed and implemented a ROS2-based state machine in C++ for a large-scale (40’x4’x3’) robotic gantry system that moves on an overhead rail system, leveraging advanced C++ features such as templating and multi-threading within a distributed system.",
-        // TODO: Fix this section
-        "Designed and implemented software architecture components for a greenhouse environmental control system, including data collection, processing, and database storage, as well as API interfaces and environmental control logic to automate actuator behavior.",
+        "Architected an LLM-driven framework for architecture design space exploration, integrating large language models with hardware simulators to enable autonomous analysis and iterative configuration proposals",
+        "Designed a modular Python tool for flexible integration of architecture models and simulators, enabling reuse and extension across future design efforts",
       ],
     },
     {
       id: 2,
+      company: "Applied Invention",
+      role: "Software Engineer",
+      dates: "Aug 2023 - Mar 2025",
+      content: [
+        "Designed and implemented a ROS2-based state machine in C++ for a large-scale (40’x4’x3’) robotic gantry system that moves on an overhead rail system, leveraging advanced C++ features such as templating and multi-threading within a distributed system.",
+        "Designed and implemented software architecture components for a greenhouse environmental control system, including data collection, processing, and database storage, as well as API interfaces and environmental control logic to automate actuator behavior.",
+      ],
+    },
+    {
+      id: 3,
       company: "Meta",
       role: "Software Engineer Intern",
       dates: "May 2022 - Aug 2022",
@@ -54,8 +63,8 @@ function ExperienceSection() {
       ],
     },
     {
-      id: 3,
-      company: "NVIDIA",
+      id: 4,
+      company: "NVIDIA (2021)",
       role: "Computer Architecture Intern",
       dates: "May 2021 - Aug 2021",
       content: [
@@ -88,7 +97,11 @@ function ExperienceSection() {
                 <div key={experience.id} className="tab-detail active">
                   <h2>
                     {experience.role} @{" "}
-                    <span className="company">{experience.company}</span>
+                    <span className="company">
+                      {experience.company.includes("(")
+                        ? experience.company.split("(")[0].trim()
+                        : experience.company}
+                    </span>
                   </h2>
                   <h3>{experience.dates}</h3>
                   <ul className="experience-bullets">

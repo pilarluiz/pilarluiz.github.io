fddb9102def26399c111e662e98782a634789554
Pilar Luiz <pluiz@usc.edu>
2025-08-22 17:26:56 -0500

    Remove vestige things

--- pilarluiz/src/App.js
+++ pilarluiz/src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
-import Services from "./components/pages/Services";
-import SignUp from "./components/pages/SignUp";
 import About from "./components/pages/About";
 
 function App() {
@@ -14,8 +12,6 @@ function App() {
         <Routes>
           {/* <Home /> -- Pass React.element; Home -- pass function */}
           <Route path="/" exact element={<Home />} />
-          <Route path="/services" exact element={<Services />} />
-          <Route path="/sign-up" exact element={<SignUp />} />
           <Route path="/about" exact element={<About />} />
         </Routes>
       </Router>
--- pilarluiz/src/components/HeroSection.js
+++ pilarluiz/src/components/HeroSection.js
@@ -8,14 +8,11 @@ function HeroSection() {
   return (
     <div className="hero-container">
       {/* TODO: flowers that rotate when you scroll */}
-      {/* <video src="/videos/video-2.mp4" autoPlay loop muted /> */}
-      {/* <span className="cout">std::cout &lt;&lt;</span> */}
       <h1>
         Pilar
         <br />
         Luiz
       </h1>
-      {/* <span className="endl">&lt;&lt; std::endl;</span> */}
     </div>
   );
 }

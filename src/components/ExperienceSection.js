319b1aeb332c80a99ceea41ff2258920efbd430c
Pilar Luiz <pluiz@usc.edu>
2025-08-23 12:40:19 -0500

    styling updates

--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -1,30 +1,3 @@
-// import React, { useState } from "react";
-
-// function ExperienceSection() {
-//   const [isHovered, setIsHovered] = useState(false);
-
-//   return (
-//     <div
-//       style={{
-//         width: isHovered ? 200 : 100,
-//         // height: isHovered ? 200 : 100,
-//         backgroundColor: "lightblue",
-//         transition: "width 0.3s",
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         cursor: "pointer",
-//       }}
-//       onMouseEnter={() => setIsHovered(true)}
-//       onMouseLeave={() => setIsHovered(false)}
-//     >
-//       Hover Me!
-//     </div>
-//   );
-// }
-
-// export default ExperienceSection;
-
 import React, { useState } from "react";
 import "./ExperienceSection.css";
 
@@ -48,8 +21,8 @@ function ExperienceSection() {
       role: "Software Engineer",
       dates: "Aug 2023 - Mar 2025",
       content: [
-        "Designed and implemented a ROS2-based state machine in C++ for a large-scale (40’x4’x3’) robotic gantry system that moves on an overhead rail system, leveraging advanced C++ features such as templating and multi-threading within a distributed system.",
-        "Designed and implemented software architecture components for a greenhouse environmental control system, including data collection, processing, and database storage, as well as API interfaces and environmental control logic to automate actuator behavior.",
+        "Designed and implemented a ROS2-based state machine in C++ for a large-scale (40’x4’x3’) robotic gantry system that moves on an overhead rail system, leveraging advanced C++ features such as templating and multi-threading within a distributed system",
+        "Designed and implemented software architecture components for a greenhouse environmental control system, including data collection, processing, and database storage, as well as API interfaces and environmental control logic to automate actuator behavior",
       ],
     },
     {
@@ -58,8 +31,8 @@ function ExperienceSection() {
       role: "Software Engineer Intern",
       dates: "May 2022 - Aug 2022",
       content: [
-        "Increased team impact by developing full-stack software that processes A/B testing data in SQL databases using Python and intelligibly displays information on React front-end.",
-        "Profiled Facebook Android application plugins and performance optimized code to generate a 12% speedup in app load time for over 50 million users.",
+        "Increased team impact by developing full-stack software that processes A/B testing data in SQL databases using Python and intelligibly displays information on React front-end",
+        "Profiled Facebook Android application plugins and performance optimized code to generate a 12% speedup in app load time for over 50 million users",
       ],
     },
     {
@@ -68,8 +41,8 @@ function ExperienceSection() {
       role: "Computer Architecture Intern",
       dates: "May 2021 - Aug 2021",
       content: [
-        "Designed tests that exploited specific streaming multiprocessor (SM) behaviors, such as consistently hitting/missing in the multi-level cache hierarchy, long graphics pipeline latencies, and massive parallelism from the SIMD architecture.",
-        "Developed a pipeline for running comparative power analysis studies using new tests and collected power measurements for various SM subunits, enabling the team to identify SM energy inefficiencies, discuss architectural tradeoffs, and explore redesign strategies for future GPUs.",
+        "Designed tests that exploited specific streaming multiprocessor (SM) behaviors, such as consistently hitting/missing in the multi-level cache hierarchy, long graphics pipeline latencies, and massive parallelism from the SIMD architecture",
+        "Developed a pipeline for running comparative power analysis studies using new tests and collected power measurements for various SM subunits, enabling the team to identify SM energy inefficiencies, discuss architectural tradeoffs, and explore redesign strategies for future GPUs",
       ],
     },
     // Add more experiences as needed
